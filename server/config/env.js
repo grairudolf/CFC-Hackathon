17edbe0a51fbe7d7e3ba44f3816500757406efb9
Grai Rudolf <rtech777r@gmail.com>
2025-05-30 18:04:58 +0100

    Database URI defining

--- server/config/env.js
+++ server/config/env.js
@@ -4,7 +4,7 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const {
   PORT = 5000,
-  DB_URI = "mongodb://localhost:27017/db.js",
+  DB_URI = "mongodb://localhost:27017/bookstore",
   JWT_SECRET,
   JWT_EXPIRATION,
 } = process.env;
